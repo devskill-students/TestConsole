8576c5de619011eafba65122fde21dce67fac0bc
Pranto <sabbiryellow@gmail.com>
2021-04-10 15:38:49 +0600

    Add project files.

---
+++ TestConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your Name?");
+            string ans = Console.ReadLine();
+            Console.WriteLine("Congratulations! You are registered " + ans);
+            Console.WriteLine(ans + " has " + ans.Length + " characters!");
+        }
+    }
+}
