e8c651ffb8419eb54b855ddebcee47afff065cdf
Pranto <sabbiryellow@gmail.com>
2021-04-10 15:54:54 +0600

    Calculator

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -6,10 +6,38 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your Name?");
-            string ans = Console.ReadLine();
-            Console.WriteLine("Congratulations! You are registered " + ans);
-            Console.WriteLine(ans + " has " + ans.Length + " characters!");
+            Console.WriteLine("Enter First Number : ");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second Number : ");
+            int secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Choose a Sign : (+)(-)(*)(/) ");
+            string sign = Console.ReadLine();
+
+            int add = firstNumber + secondNumber;
+            int minus = firstNumber - secondNumber;
+            int multiply = firstNumber * secondNumber;
+            int divide = firstNumber / secondNumber;
+
+            if (sign == "+")
+            {
+                Console.WriteLine("The Result is : " + add);
+            }
+
+            else if (sign == "-")
+            {
+                Console.WriteLine("The Result is : " + minus);
+            }
+
+            else if (sign == "-")
+            {
+                Console.WriteLine("The Result is : " + multiply);
+            }
+
+            else if (sign == "-")
+            {
+                Console.WriteLine("The Result is : " + divide);
+            }
         }
+
     }
 }
